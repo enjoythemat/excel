ce1be3d261e86633834f04d94e4485c4143f7b42
enjoythemat <gg_man91@mail.ru>
2021-10-10 18:40:01 +0500

    Creating unit testing env

--- webpack.config.js
+++ webpack.config.js
@@ -13,10 +13,7 @@ const filename = ext => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`
 const jsLoaders = () => {
   const loaders = [
     {
-      loader: 'babel-loader',
-      options: {
-        presets: ['@babel/preset-env']
-      }
+      loader: 'babel-loader'
     }
   ]
 
